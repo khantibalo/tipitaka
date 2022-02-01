272ddaacfe26b416931fb1d61d4989703a032cd4
khantibalo <khantibalo@khantibalo.com>
2022-02-01 21:32:25 +0300

    recipe update

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,15 +3,12 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Resource\FileResource;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+<<<<<<< ours
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
@@ -51,4 +48,6 @@ class Kernel extends BaseKernel
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
     }
+=======
+>>>>>>> theirs
 }
