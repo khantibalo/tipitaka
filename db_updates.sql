1752579c8bb9e736d3b727a44bd119c98be0afe1
khantibalo <khantibalo@khantibalo.com>
2025-05-12 15:56:22 +0300

    db update for last commit

--- db_updates.sql
+++ db_updates.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `tipitaka_tags` CHANGE `paliname` `paliname` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL; 
+ALTER TABLE `tipitaka_collection_items` ADD `notes_bottom` TEXT NULL AFTER `default_view`; 
